706631a8a5438d3c38b2f2a37ff421bfde7efd22
Svyatoslav Reentenko <rsvato@gmail.com>
2022-08-23 00:13:52 +0300

    Initial import

---
+++ src/main/java/net/quanzy/authzmod/AuthzMod.java
@@ -0,0 +1,14 @@
+package net.quanzy.authzmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AuthzMod implements ModInitializer {
+    public static final Logger logger = LoggerFactory.getLogger("authz");
+
+    @Override
+    public void onInitialize() {
+        logger.info("In onInitialize()");
+    }
+}
---
+++ src/main/java/net/quanzy/authzmod/mixin/AuthzMixin.java
@@ -0,0 +1,20 @@
+package net.quanzy.authzmod.mixin;
+
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class AuthzMixin {
+    public static final Logger logger = LoggerFactory.getLogger(AuthzMixin.class);
+
+    @Inject(at = @At("HEAD"), method = "init()V")
+    private void init(CallbackInfo info) {
+        logger.info("This line is printed by authz mixin!");
+    }
+
+}
